4958d1b2ef46568e7c3e833670d548f23d52c358
thivaapbi <133944475+thivaapbi@users.noreply.github.com>
2023-05-18 11:28:44 +0530

    Update streamlit_app.py
    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('PBI')
-streamlit.header('hi')
-streamlit.text('one')
-streamlit.text('two')
-streamlit.text('three')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
