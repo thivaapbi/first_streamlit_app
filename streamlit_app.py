61d2745fe99667e5bdc07407453e20b2dc77dcc1
thivaapbi <133944475+thivaapbi@users.noreply.github.com>
2023-05-19 23:41:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,1 @@
-import streamlit
-streamlit.tile('My')
-streamlit.header('Breakfast Favourite')
-streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+hihihih
